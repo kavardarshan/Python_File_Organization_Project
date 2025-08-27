93e603d41aae6d1dd7e7cbc47ea3da0a8f091730
Darshan_Kavar <kavardarshan81@gmail.com>
2025-08-27 13:33:52 +0530

    Add files via upload

--- File_Organization.py
+++ File_Organization.py
@@ -1,27 +1,21 @@
-import os, platform, subprocess, datetime
+import os
+import shutil
 
-# current time
-now = datetime.datetime.now()
+folder = "C:/Users/Downloads"
 
-if platform.system() == "Windows":
-    cpu_out = subprocess.check_output(["wmic", "cpu", "get", "loadpercentage"]).decode().split()
-    cpu = cpu_out[1] if len(cpu_out) > 1 else "0"
+for file in os.listdir(folder):
+    path = os.path.join(folder, file)
 
-    # get memory info
-    mem_out = subprocess.check_output([
-        "wmic", "OS", "get", "FreePhysicalMemory,TotalVisibleMemorySize", "/Value"
-    ]).decode().split()
-    free_mem = int([x.split('=')[1] for x in mem_out if x.startswith("FreePhysicalMemory")][0])
-    total_mem = int([x.split('=')[1] for x in mem_out if x.startswith("TotalVisibleMemorySize")][0])
-    mem = round((1 - free_mem / total_mem) * 100, 2)
 
-else:
-    cpu_line = subprocess.getoutput("top -bn1 | grep 'Cpu(s)'")
-    cpu = cpu_line.split('%')[0].split()[-1]
+    if os.path.isdir(path):
+        continue
 
-    mem_line = subprocess.getoutput("free -m | grep Mem").split()
-    mem = round(int(mem_line[2]) / int(mem_line[1]) * 100, 2)
+    ext = file.split('.')[-1]
 
-# save to file
-with open("system_log.txt", "a") as f:
-    f.write(f"{now} - CPU: {cpu}% | Memory: {mem}%\n")
+    ext_folder = os.path.join(folder, ext)
+    if not os.path.exists(ext_folder):
+        os.mkdir(ext_folder)
+
+    shutil.move(path, os.path.join(ext_folder, file))
+
+print("Files organized by type!")
